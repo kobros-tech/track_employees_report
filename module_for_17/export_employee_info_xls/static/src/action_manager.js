934d975dfef1429587d6298b1d409b106c9ab737
kobros-tech <alkobroslymohamed@gmail.com>
2024-04-11 20:11:43 +0300

    make fields dependencies for ExportExcel component and make it disabled

--- module_for_17/export_employee_info_xls/static/src/action_manager.js
+++ module_for_17/export_employee_info_xls/static/src/action_manager.js
@@ -12,14 +12,23 @@ class PrintExcel extends Component {
         this.orm = useService("orm");
     }
 
+    get showButton() {
+        if (this.props.record.data.validate && this.props.record.data.from_date && this.props.record.data.to_date) {
+            return false
+        }
+        else {
+            return true
+        }
+    }
+
     async onDownloadButtonClicked() {
         // console.log(this.env.model.root.data.project_ids.records);
 
-        const projects = this.env.model.root.data.project_ids.records
-        const employees = this.env.model.root.data.target_employees_ids.records
-        const from_date = this.env.model.root.data.from_date.c
-        const to_date = this.env.model.root.data.to_date.c
-        const validate = this.env.model.root.data.validate
+        const projects = this.props.record.data.project_ids.records
+        const employees = this.props.record.data.target_employees_ids.records
+        const from_date = this.props.record.data.from_date.c
+        const to_date = this.props.record.data.to_date.c
+        const validate = this.props.record.data.validate
         
         const projects_ids = projects.map((project) =>{
             return project.data;
@@ -36,14 +45,6 @@ class PrintExcel extends Component {
             validate: validate,
         };
 
-        console.log(data);
-        console.log("times", from_date, to_date);
-        console.log("Projects:", projects);
-        console.log("employees:", employees);
-        console.log("target_employees_ids:", target_employees_ids);
-        console.log("projects_ids:", projects_ids);
-        console.log("downloading excel file");
-
         download({
             url: '/employee_xlsx_report',
             data: { data: JSON.stringify(data) },
@@ -56,6 +57,11 @@ PrintExcel.template = "export_employee_info_xls.PrintExcel";
 PrintExcel.props = {
     ...standardWidgetProps,
 };
+PrintExcel.fieldDependencies = [
+    { name: "from_date", type: "date" },
+    { name: "to_date", type: "date" },
+    { name: "validate", type: "selection" },
+];
 
 const printExcel = {
     component: PrintExcel,
